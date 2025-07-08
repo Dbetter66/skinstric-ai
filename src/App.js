ec444daa842a4cada58db2e0c1cbf999114b4447
Daniel <dbetter66@gmail.com>
2025-07-08 17:20:35 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import Home from './components/Home';
+import Start from './components/Start';
+import Analysis from './components/Analysis';
+
+// ... (Your page components like HomePage, AboutPage, etc.)
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+
+
+        <Router>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/start" element={<Start />} />
+        <Route path="Analysis" element={<Analysis />} />
+      </Routes>
+      </Router>
+      
   );
 }
 
 export default App;
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+function Home () {
+    return (
+
+        <div className="main-heading">
+        <div className="flex justify-center items-center h-screen text-[60px] ">
+            <h1>Sophisticated Skincare</h1>
+        </div>
+</div>
+
+
+    );
+}
+
+export default Home;
