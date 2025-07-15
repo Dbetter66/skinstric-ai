fd3d636412ec31143cc89388968a062678b16dda
Daniel <dbetter66@gmail.com>
2025-07-15 14:12:08 -0600

    working on adding image file as a base64 to the api

---
+++ src/components/Result.jsx
@@ -0,0 +1,198 @@
+import React, { useRef } from 'react';
+
+function Result() {
+
+    const fileInputRef = useRef(null);
+
+    const handleButtonClick = () => {
+        fileInputRef.current.click(); // Programmatically click the hidden input
+      };
+    
+      const handleFileChange = (event) => {
+        const selectedFiles = event.target.files; // Get the selected files
+        // Do something with the selected files, like uploading them or processing their content
+        console.log("Selected files:", selectedFiles);
+      };
+
+  return (
+      <div className="min-h-[80vh] flex flex-col bg-white relative md:pt-[64px] justify-center">
+        <div className="absolute top-2 left-9 md:left-8 text-left">
+          <div className="flex flex-col bg-white relative md:pt-[64px] justify-center">
+            <p className="font-semibold text-xs md:text-sm">
+              TO START ANALYSIS
+            </p>
+          </div>
+        </div>
+        <div className="flex-[0.4] md:flex-1 flex flex-col md:flex-row items-center xl:justify-center relative mb-0 md:mb-30 space-y-[-20px] md:space-y-0">
+          <div className="relative md:absolute md:left-[55%] lg:left-[50%] xl:left-[40%] md:-translate-y-[0%] -translate-y-[1%] md:-translate-x-full flex flex-col items-center justify-center">
+            <div className="w-[270px] h-[270px] md:w-[482px] md:h-[482px]"></div>
+
+            <img
+              alt="largediamond"
+              loading="lazy"
+              width="762"
+              height="762"
+              decoding="async"
+              data-nimg="1"
+              className="absolute w-[480px] h-[480px] md:w-[762px] md:h-[762px] animate-spin-slow [animation-duration:50s] rotate-190"
+              src="./largediamond.svg"
+            ></img>
+            <img
+              alt="mediumdiamon"
+              loading="lazy"
+              width="682"
+              height="682"
+              decoding="async"
+              data-nimg="1"
+              className="absolute w-[400px] h-[400px] md:w-[682px] md:h-[682px] animate-spin-slower [animation-duration:70s] rotate-185"
+              src="./mediumdiamond.svg"
+            ></img>
+            <img
+              alt="smalldiamond"
+              loading="lazy"
+              width="602"
+              height="602"
+              decoding="async"
+              data-nimg="1"
+              className="absolute w-[320px] h-[320px] md:w-[602px] md:h-[602px] animate-spin-slowest [animation-duration:85s]"
+              src="./smalldiamond.svg"
+            ></img>
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+              <div className="absolute inset-0 flex flex-col items-center justify-center">
+                <img
+                  alt="shutter-icon"
+                  loading="lazy"
+                  width="136"
+                  height="136"
+                  decoding="async"
+                  data-nimg="1"
+                  className="absolute w-[100px] h-[100px] md:w-[136px] md:h-[136px] transform transition-transform duration-300 hover:scale-110 ease-in-out cursor-pointer"
+                  src="./shutter-icon.svg"
+                ></img>
+                <div className="absolute bottom-[1%] right-[90px] md:top-[30.9%] md:right-[-12px] translate-y-[-20px]">
+                  <p className="text-xs md:text-sm font-normal mt-1 leading-[24px]">
+                    ALLOW A.I
+                    <br />
+                    TO SCAN YOUR FACE
+                  </p>
+                  <img
+                    alt="Scan Line"
+                    loading="lazy"
+                    width="66"
+                    height="59"
+                    decoding="async"
+                    data-nimg="1"
+                    className="absolute hidden md:block md:right-[143px] md:top-[20px]"
+                    src="./scanline.svg"
+                  ></img>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div className="relative md:absolute md:left-[45%] lg:left-[50%] xl:left-[55%] flex flex-col items-center mt-12 md:mt-0 justify-center md:-translate-y-[0%] -translate-y-[10%] transition-opacity duration-300 opacity-100">
+            <div className="w-[270px] h-[270px] md:w-[482px] md:h-[482px]"></div>
+            <img
+              alt="largediamond"
+              loading="lazy"
+              width="762"
+              height="762"
+              decoding="async"
+              data-nimg="1"
+              className="absolute w-[480px] h-[480px] md:w-[762px] md:h-[762px] animate-spin-slow [animation-duration:50s] rotate-190"
+              src="./largediamond.svg"
+            ></img>
+            <img
+              alt="mediumdiamon"
+              loading="lazy"
+              width="682"
+              height="682"
+              decoding="async"
+              data-nimg="1"
+              className="absolute w-[400px] h-[400px] md:w-[682px] md:h-[682px] animate-spin-slower [animation-duration:70s] rotate-185"
+              src="./mediumdiamond.svg"
+            ></img>
+            <img
+              alt="smalldiamond"
+              loading="lazy"
+              width="602"
+              height="602"
+              decoding="async"
+              data-nimg="1"
+              className="absolute w-[320px] h-[320px] md:w-[602px] md:h-[602px] animate-spin-slowest [animation-duration:85s]"
+              src="./smalldiamond.svg"
+            ></img>
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+            <div className="absolute inset-0 flex flex-col items-center justify-center">
+                <button onClick={handleButtonClick}>
+                <input
+        type="file"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        style={{ display: 'none' }} // Hide the default file input
+        multiple // Allow selecting multiple files (optional)
+        // accept=".pdf, .doc, .docx" // Restrict file types (optional)
+      />
+              <img
+                alt="Photo Upload Icon"
+                loading="lazy"
+                width="136"
+                decoding="async"
+                data-nimg="1"
+                className="w-[100px] h-[100px] md:w-[136px] md:h-[136px]  transform transition-transform duration-300 hover:scale-110 ease-in-out cursor-pointer"
+                src="./photoupload.svg"
+              ></img>
+              <div className="absolute top-[75%] md:top-[70%] md:left-[17px] translate-y-[-10px]">
+                <p className="text-[12px] md:text-[14px] font-normal mt-2 leading-[24px] text-right">
+                    ALLOW A.I. <br />
+                    ACCESS GALLERY
+                </p>
+               
+                <img
+                    alt="Scan Line"
+                    loading="lazy"
+                    width="66"
+                    height="59"
+                    decoding="async"
+                    data-nimg="1"
+                    className="absolute hidden md:block md:left-[120px] md:bottom-[39px]"
+                    src="./ailine.svg"
+                  ></img>
+              </div></button>
+           </div> </div>
+          </div>
+          <div className="absolute top-[-75px] right-7 md:top-[-50px] md:right-8 transition-opacity duration-300 opacity-100">
+            <h1 className="text-xs md:text-sm font-normal mb-1">PREVIEW</h1>
+            <div className="w-24 h-24 md:w-32 md:h-32 border border-gray-300 overflow-hidden"></div>
+          </div>
+          <input accept="image/*" className="hidden" type="file"></input>
+        </div>
+        <div className="pt-4 md:pt-0 pb-8 bg-white sticky md:static bottom-30.5 mb-0 md:mb-0">
+            <div className="absolute bottom-8 w-full flex justify-between md:px-9 px-13">
+            <div className="bottom-38.5 md:bottom-8 w-full flex justify-between md:px-9 px-13 sm:bottom-10">
+        <a className="inset-0" aria-label="Back" href="/testing">
+          <div>
+            <div className="relative w-12 h-12 flex items-center justify-center border border-[#1A1B1C] rotate-45 scale-[1] sm:hidden">
+              <span className="rotate-[-45deg] text-xs font-semibold sm:hidden">
+                BACK
+              </span>
+            </div>
+            <div className="group hidden sm:flex flex-row relative justify-center items-center">
+              <div className="w-12 h-12 hidden sm:flex justify-center border border-[#1A1B1C] rotate-45 scale-[0.85] group-hover:scale-[0.92] ease duration-300"></div>
+              <span className="absolute left-[15px] bottom-[13px] scale-[0.9] rotate-180 hidden sm:block group-hover:scale-[0.92] ease duration-300">
+                ▶
+              </span>
+              <span className="text-sm font-semibold hidden sm:block ml-6 ">
+                BACK
+              </span>
+            </div>
+          </div>
+        </a>
+        </div>
+            </div>
+        </div>
+      </div>
+  );
+}
+
+export default Result;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+
+      transitionProperty: {
+        'spacing': 'margin, padding',
+        'width-scale': 'width, transform', // For effects where both width and scale change
+      },
+      transitionDuration: {
+        'extra-slow': '1500ms', // Custom longer transition
+      },
+      transitionTimingFunction: {
+        'bounce-soft': 'cubic-bezier(0.68, -0.55, 0.265, 1.55)', // Custom timing function
+      },
       // Add your custom keyframes here
       keyframes: {
         'spin-slow':  {
@@ -18,12 +29,31 @@ module.exports = {
           '0%': { transform: 'rotate(0deg)' },
           '100%': { transform: 'rotate(360deg)' },
         },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        shine: {
+          '100%': { left: '125%' }, // For a "shining" effect
+        },
+        glow: {
+          '0%, 100%': { boxShadow: '0 0 10px #ff00ff' },
+          '50%': { boxShadow: '0 0 20px #ff00ff' },
+        },
+        pulseSmall: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.05)' },
+        },
       },
       // Add your custom animation utilities here
       animation: {
         'spin-slow': 'spin-slow 10s linear infinite',    // Adjust duration as needed
         'spin-slower': 'spin-slower 15s linear infinite', // Adjust duration as needed
-        'spin-slowest': 'spin-slowest 20s linear infinite',// Adjust duration as needed
+        'spin-slowest': 'spin-slowest 20s linear infinite',
+        'fade-in': 'fadeIn 2s ease-out',
+        'shine': 'shine 1s forwards', // Using forwards to keep the final state
+        'glow': 'glow 1.5s infinite',
+        'pulse-small': 'pulseSmall 1s infinite ease-in-out',// Adjust duration as needed
       },
     },
   },
